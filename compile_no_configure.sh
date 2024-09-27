f9ee341ce1e879beecd6a17fcda664b624d43b5e
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-09-27 12:29:57 +0200

    add: added single file compilation scripts and  modified Makefile

---
+++ compile_no_configure.sh
@@ -0,0 +1,13 @@
+mkdir -p build/bin
+make clean > /dev/null
+rm build/bin/tmux
+opt="$(echo $1 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
+make EXTRA_CFLAGS=" $opt" -j -n &> log_make.txt
+if ! make EXTRA_CFLAGS="$opt" -j ; then
+	echo "error make"
+	exit 1
+fi
+if ! cp tmux build/bin/tmux; then
+	echo "error copying binary"
+	exit 1
+fi
---
+++ compile_to_asm.sh
@@ -0,0 +1,7 @@
+# $1: name of the c file to compile to assembly
+# $2 output path
+opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
+if ! gcc -masm=intel -iquote. -std=gnu99 -O2 -g -Wno-long-long -Wall -W -Wformat=2 -Wmissing-prototypes -Wstrict-prototypes -Wmissing-declarations -Wwrite-strings -Wshadow -Wpointer-arith -Wsign-compare -Wundef -Wbad-function-cast -Winline -Wcast-align -Wdeclaration-after-statement -Wno-pointer-sign -Wno-attributes -Wno-unused-result -Wno-format-y2k $opt -MT server.o -MD -MP -MF $depbase.Tpo -S -o "$2" "$1"; then
+	echo "error compile to asm"
+	exit 1
+fi
